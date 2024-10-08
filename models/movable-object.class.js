2002f27d21d956772f09218894592d8647f5a4e8
FlorianZunke <florianzunke@gmx.de>
2024-10-08 20:10:58 +0200

    added draw function with drawing enemies and character

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,14 +1,20 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 150;
     img;
+    height = 250;
+    width = 250;
+
+    loadImage(path) {
+        this.img  = new Image();
+        this.img.src = path;
+    }
 
     moveRight() {
         console.log('Moving right');
-        
     }
 
-    constructor(parameters) {
-        
+    moveLeft() {
+
     }
 }
---
+++ models/puffer_fish.class.js
@@ -0,0 +1,8 @@
+class PufferFish extends MovableObject{
+
+    constructor() {
+        super().loadImage('img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png')
+    }
+
+    
+}
