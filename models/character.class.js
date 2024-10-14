0d264db640e14e454ae1a7ccf3af26713abfa094
FlorianZunke <florianzunke@gmx.de>
2024-10-14 20:06:45 +0200

    added function to move Character

--- models/character.class.js
+++ models/character.class.js
@@ -1,11 +1,64 @@
 class Character extends MovableObject{
-    
+    world;
+    speed = 5;
+    IMAGES_SWIM = [
+        'img/1.Sharkie/1.IDLE/1.png',
+        'img/1.Sharkie/1.IDLE/2.png',
+        'img/1.Sharkie/1.IDLE/3.png',
+        'img/1.Sharkie/1.IDLE/4.png',
+        'img/1.Sharkie/1.IDLE/5.png',
+        'img/1.Sharkie/1.IDLE/6.png',
+        'img/1.Sharkie/1.IDLE/7.png',
+        'img/1.Sharkie/1.IDLE/8.png',
+        'img/1.Sharkie/1.IDLE/9.png',
+        'img/1.Sharkie/1.IDLE/10.png',
+        'img/1.Sharkie/1.IDLE/11.png',
+        'img/1.Sharkie/1.IDLE/12.png',
+        'img/1.Sharkie/1.IDLE/13.png',
+        'img/1.Sharkie/1.IDLE/14.png',
+        'img/1.Sharkie/1.IDLE/15.png',
+        'img/1.Sharkie/1.IDLE/16.png',
+        'img/1.Sharkie/1.IDLE/17.png',
+        'img/1.Sharkie/1.IDLE/18.png',
+    ];
 
     constructor() {
-        super().loadImage('img/1.Sharkie/1.IDLE/1.png')
+        super().loadImage('img/1.Sharkie/1.IDLE/1.png'),
+        this.loadImages(this.IMAGES_SWIM);
+
+        this.animate();
     }
 
-    swimUp() {
+    animate() {
+        setInterval(() => {
 
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.UP) {
+                let i = this.currentImage % this.IMAGES_SWIM.length;
+                let path = this.IMAGES_SWIM[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 150);
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+
+            if (this.world.keyboard.UP) {
+                this.y -= this.speed;
+            }
+
+            if (this.world.keyboard.DOWN) {
+                this.y += this.speed;
+            }
+        }, 1000 / 60);
     }
 }
